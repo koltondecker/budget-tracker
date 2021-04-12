d94af73aa60f81315b73f1d5e4529ca88cc6d7d8
Kolton Decker <deckerman25@gmail.com>
2021-04-11 22:24:43 -0500

    Updated cache files list

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,10 +2,10 @@ const FILES_TO_CACHE = [
     "/",
     "/index.html",
     "/styles.css",
+    "/index.js",
     "/manifest.webmanifest",
-    "/favicon.ico",
-    "/assets/images/icons/icon-192x192.png",
-    "/assets/images/icons/icon-512x512.png",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
 ];
 
 const CACHE_NAME = "static-cache-v2";
